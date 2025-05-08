cf3fd54a75b771b3efa1eaea06a7f1c2e6c8f775
KlowasMunik <Munikgklowas@gmail.com>
2025-05-08 11:35:09 -0700

    m

--- src/main/java/edu/miracosta/cs112/lotaria/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lotaria/HelloApplication.java
@@ -1,25 +1,33 @@
 package edu.miracosta.cs112.lotaria;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class HelloApplication extends Application {
-    private static final LoteriaCard[] LOTERIA_CARDS = {
-            new LoteriaCard("Las matematicas", "1.png", 1),
-            new LoteriaCard("Las ciencias", "2.png", 2),
-            new LoteriaCard("La Tecnología", "8.png", 8),
-            new LoteriaCard("La ingeniería", "9.png", 9),
-    };
+import java.io.IOException;
+import java.net.URL;
 
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 320, 240);
-        stage.setTitle("Loteria!");
-        stage.setScene(scene);
-        stage.show();
+        try {
+            // Get the FXML file as a resource
+            URL fxmlUrl = getClass().getResource("/Users/munikklowas/IdeaProjects/l08-loteria-KlowasMunik/src/main/resources/edu/miracosta/cs112/lotaria/loteriaintellij/hello-view.fxml");
+
+            // Load the FXML file
+            Parent root = FXMLLoader.load(fxmlUrl);
+
+            // Create and show the scene
+            Scene scene = new Scene(root, 350, 500);
+            stage.setTitle("EChALE STEM Loteria!");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error loading FXML: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
